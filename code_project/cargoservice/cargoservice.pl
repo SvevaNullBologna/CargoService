753f34782d3395e11c4635bb71919b9cfc29b110
SvevaNullBologna <silviaangela.carollo@studio.unibo.it>
2025-07-30 14:09:45 +0200

    rebuilt project. LET'S HOPE

---
+++ code_project/cargoservice/cargoservice.pl
@@ -0,0 +1,31 @@
+%====================================================================================
+% cargoservice description   
+%====================================================================================
+request( loadrequest, loadrequest(PID) ).
+request( getProduct, product(ID) ).
+reply( getProductAnswer, product(JSonString) ).  %%for getProduct
+dispatch( accepted, accepted(PID,Weight,Slot) ).
+dispatch( refused, refused(PID,Weight) ).
+event( productDetected, productDetected(T) ).
+event( anomalyDetected, anomalyDetected(T) ).
+event( anomalyFixed, anomalyFixed(T) ).
+dispatch( command, command(X,Y,DIR) ).
+request( engage, engage(device,length) ).
+request( moverobot, moverobot(x,y) ).
+dispatch( setdirection, dir(D) ).
+dispatch( cmd, cmd(C) ).
+event( alarm, alarm(reason) ).
+dispatch( update, update(U) ).
+event( finishedtransport, finishedtransport(T) ).
+%====================================================================================
+context(ctx_cargoservice, "localhost",  "TCP", "8000").
+context(ctx_productservice, "cargoserviceqak",  "TCP", "8111").
+context(ctx_basicrobot, "basicrobot24",  "TCP", "8020").
+ qactor( basicrobot, ctx_basicrobot, "external").
+  qactor( productservice, ctx_productservice, "external").
+  qactor( cargoservice, ctx_cargoservice, "it.unibo.cargoservice.Cargoservice").
+ static(cargoservice).
+  qactor( cargorobot, ctx_cargoservice, "it.unibo.cargorobot.Cargorobot").
+ static(cargorobot).
+  qactor( sonarmock, ctx_cargoservice, "it.unibo.sonarmock.Sonarmock").
+ static(sonarmock).
