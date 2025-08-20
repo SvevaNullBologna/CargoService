91df53811975f895b8e01181533c466d7f53d2be
SvevaNullBologna <silviaangela.carollo@studio.unibo.it>
2025-08-20 16:37:09 +0200

    working on sprint 3

---
+++ sprint3/cargoservice/cargoservice.pl
@@ -0,0 +1,38 @@
+%====================================================================================
+% cargoservice description   
+%====================================================================================
+mqttBroker("mosquittoalone", "1883", "unibo/qak/events").
+request( loadrequest, loadrequest(PID) ).
+request( getProduct, product(ID) ).
+reply( getProductAnswer, product(JSonString) ).  %%for getProduct
+dispatch( accepted, accepted(PID,Weight,Slot) ).
+dispatch( refused, refused(PID,Weight) ).
+event( productDetected, productDetected(T) ).
+event( anomalyDetected, anomalyDetected(T) ).
+event( anomalyFixed, anomalyFixed(T) ).
+dispatch( command, command(X,Y,DIR) ).
+event( deliveredToSlot, deliveredToSlot(T) ).
+request( engage, engage(device,length) ).
+request( moverobot, moverobot(x,y) ).
+dispatch( setdirection, dir(D) ).
+event( alarm, alarm(reason) ).
+event( info, info(x) ).
+dispatch( update, update(U) ).
+event( finishedtransport, finishedtransport(T) ).
+%====================================================================================
+context(ctx_cargoservice, "localhost",  "TCP", "8000").
+context(ctx_productservice, "127.0.0.1",  "TCP", "8111").
+context(ctx_basicrobot, "127.0.0.1",  "TCP", "8020").
+context(ctx_sonarservice, "127.0.0.1",  "TCP", "8004").
+ qactor( basicrobot, ctx_basicrobot, "external").
+  qactor( productservice, ctx_productservice, "external").
+  qactor( companysimulator, ctx_cargoservice, "it.unibo.companysimulator.Companysimulator").
+ static(companysimulator).
+  qactor( cargoservice, ctx_cargoservice, "it.unibo.cargoservice.Cargoservice").
+ static(cargoservice).
+  qactor( cargorobot, ctx_cargoservice, "it.unibo.cargorobot.Cargorobot").
+ static(cargorobot).
+  qactor( hold, ctx_cargoservice, "it.unibo.hold.Hold").
+ static(hold).
+  qactor( webguimock, ctx_cargoservice, "it.unibo.webguimock.Webguimock").
+ static(webguimock).
---
+++ sprint3/cargoservice/src/main/java/Hold.java
@@ -0,0 +1,16 @@
+package main.java;
+
+
+
+public class Hold{
+	private int Cur_HoldWeight
+	
+	public Hold() {
+		Cur_HoldWeight = 0; 
+	}
+	
+	
+	public int get_current_weight() {
+		return this.Cur_HoldWeight;
+	}
+}
