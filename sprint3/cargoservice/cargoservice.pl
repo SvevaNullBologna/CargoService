4646baa11bed9877e204cd877c51ca95b3940f2a
SvevaNullBologna <silviaangela.carollo@studio.unibo.it>
2025-08-24 17:58:18 +0200

    work in progress

--- sprint3/cargoservice/cargoservice.pl
+++ sprint3/cargoservice/cargoservice.pl
@@ -9,17 +9,17 @@ event( productDetected, productDetected(T) ).
 event( anomalyDetected, anomalyDetected(T) ).
 event( anomalyFixed, anomalyFixed(T) ).
 dispatch( command, command(X,Y,DIR) ).
+request( checkIfFits, checkIfFits(PID,Weight) ).
 event( deliveredToSlot, deliveredToSlot(T) ).
+event( finishedtransport, finishedtransport(T) ).
 request( engage, engage(device,length) ).
 request( moverobot, moverobot(x,y) ).
 dispatch( setdirection, dir(D) ).
 event( alarm, alarm(reason) ).
 event( info, info(x) ).
-dispatch( update, update(HoldJsonString) ).
-event( finishedtransport, finishedtransport(T) ).
-request( checkIfFits, checkIfFits(PID,Weight) ).
 reply( accepted, accepted(JsonString) ).  %%for checkIfFits
 reply( refused, refused(Reason) ).  %%for checkIfFits
+dispatch( update, update(HoldJsonString) ).
 %====================================================================================
 context(ctx_cargoservice, "localhost",  "TCP", "8000").
 context(ctx_productservice, "127.0.0.1",  "TCP", "8111").
@@ -35,5 +35,5 @@ context(ctx_sonarservice, "127.0.0.1",  "TCP", "8004").
  static(cargorobot).
   qactor( hold, ctx_cargoservice, "it.unibo.hold.Hold").
  static(hold).
-  qactor( webguimock, ctx_cargoservice, "it.unibo.webguimock.Webguimock").
- static(webguimock).
+  qactor( webgui, ctx_cargoservice, "it.unibo.webgui.Webgui").
+ static(webgui).
---
+++ sprint3/webgui/src/main/java/unibo/disi/webgui/WebSocketConfiguration.java
@@ -0,0 +1,17 @@
+package unibo.disi.webgui;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+public static final WebSocketHandlerDemo wshandler = new WebSocketHandlerDemo();
+public static final String wspath                  = "socket";
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+    registry.addHandler(wshandler, wspath).setAllowedOrigins("*");
+  }
+}
