666721cae9386e05ae53c6cbdf1ebade9dd0779f
SvevaNullBologna <silviaangela.carollo@studio.unibo.it>
2025-09-04 16:39:38 +0200

    fine(si spera)

---
+++ sprint3/webguifordocker/webgui/src/main/java/unibo/webgui/service/CallerService.java
@@ -0,0 +1,70 @@
+package unibo.webgui.service;
+
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import unibo.basicomm23.interfaces.IApplMessage;
+import unibo.basicomm23.interfaces.Interaction;
+import unibo.basicomm23.msg.ProtocolType;
+import unibo.basicomm23.utils.CommUtils;
+import unibo.basicomm23.utils.ConnectionFactory;
+import unibo.webgui.utils.HoldResponseParser;
+import unibo.webgui.ws.WSHandler;
+
+@RestController
+public class CallerService {
+
+    @Autowired
+    private WSHandler wsHandler;
+
+    private Interaction conn;
+
+    public CallerService() {
+        try {
+        	// usa questo da dentro i container
+        	conn = ConnectionFactory.createClientSupport23(ProtocolType.tcp, "cargoservice", "8000");
+            //conn = ConnectionFactory.createClientSupport23(ProtocolType.tcp, "127.0.0.1", "8000");
+            //conn = ConnectionFactory.createClientSupport23(ProtocolType.tcp, "localhost", "8000");
+        } catch (Exception e) {
+            System.err.println("Errore nella connessione TCP iniziale: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/caller")
+    public String callCargoservice(@RequestParam("pid") String pid) {
+        try {
+        	CommUtils.outblue("send request to cargoservice");
+        	IApplMessage getreq = CommUtils.buildRequest(
+        		    "webgui",
+        		    "sendrequest",
+        		    "sendrequest(\""+pid+"\")",  // <- nota le virgolette
+        		    "companyrequestreceiver"
+        		);
+        	IApplMessage answer = conn.request(getreq,10000);  //raises exception
+            return cleanMessage(answer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"error\":\"" + e.getMessage() + "\"}";
+        }
+    }
+    
+    
+    private String cleanMessage(IApplMessage answer) {
+    	if(answer != null) {
+            String full = answer.msgContent();
+            int start = full.indexOf('\''); 
+            int end   = full.lastIndexOf('\'');
+            if(start >= 0 && end > start) {
+                return full.substring(start+1, end);
+            } else {
+                return full;
+            }
+        } else {
+            return "late response";
+        }
+    }
+}
---
+++ sprint3/webguifordocker/webgui/src/main/java/unibo/webgui/utils/HoldResponseParser.java
@@ -0,0 +1,43 @@
+package unibo.webgui.utils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HoldResponseParser {
+
+    // Parsa una stringa JSON completa e restituisce un oggetto JSON leggibile dalla webgui.
+    public static JSONObject parseHoldState(String message) {
+    	String jsonString = null;
+	    JSONObject payload = new JSONObject();
+
+	    if (message.startsWith("'") && message.endsWith("'")) {
+	        jsonString = message.substring(1, message.length() - 1);
+	    } else if (message.startsWith("{")) {
+	        jsonString = message;
+	    } else {
+	        System.err.println("Messaggio non riconosciuto: " + message);
+	        return payload;
+	    }
+	    
+	    JSONObject original = new JSONObject(jsonString);
+	    
+	    int currentLoad = original.getInt("current_hold_weight");
+        JSONArray slotsArray = original.getJSONArray("slots");
+	    
+        
+        List<String> slotStatusList = new ArrayList<>();
+        for (int i = 0; i < slotsArray.length(); i++) {
+            JSONObject slot = slotsArray.getJSONObject(i);
+            boolean available = slot.getBoolean("available");
+            slotStatusList.add(available ? "libero" : "pieno");
+        }
+        
+        payload.put("shipLoad", currentLoad);
+        payload.put("slots", slotStatusList);
+        
+        
+        return payload;
+    }
+}
